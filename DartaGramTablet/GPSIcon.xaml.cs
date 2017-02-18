4db43b849624607121f0ea2544160750cd98e3b3
Ravi Teja Bhattiprolu <sbhattiprolu@unomaha.edu>
2017-02-18 13:57:11 -0600

    Windows Tablet application

---
+++ DartaGramTablet/GPSIcon.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Bing.Maps;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace DartaGramTablet
+{
+    public sealed partial class GPSIcon : UserControl
+    {
+        private Map _map;
+        private double _radius;
+        private const double EARTH_RADIUS_METERS = 6378137;
+
+        public GPSIcon(Map map)
+        {
+            this.InitializeComponent();
+            _map = map;
+
+            //Add a View Changed event to the map to update the accuracy circle
+            _map.ViewChanged += (s, e) =>
+            {
+                //Update the accuracy circle
+                UpdateAccuracyCircle();
+            };
+        }
+
+
+        public void SetRadius(double radiusInMeters)
+        {
+            //Store the radius value
+            _radius = radiusInMeters;
+
+            //Update the accuracy circle
+            UpdateAccuracyCircle();
+        }
+        private void UpdateAccuracyCircle()
+        {
+            if (_map != null && _radius >= 0)
+            {
+                //Calculate the ground resolution in meters/pixel
+                //Math based on http://msdn.microsoft.com/en-us/library/bb259689.aspx
+                double groundResolution = Math.Cos(_map.Center.Latitude * Math.PI / 180) *
+                    2 * Math.PI * EARTH_RADIUS_METERS / (256 * Math.Pow(2, _map.ZoomLevel));
+
+                //Calculate the radius of the accuracy circle in pixels
+                double pixelRadius = _radius / groundResolution;
+
+                //Update the accuracy circle dimensions
+                AccuracyCircle.Width = pixelRadius;
+                AccuracyCircle.Height = pixelRadius;
+
+                //Use the margin property to center the accuracy circle
+                AccuracyCircle.Margin = new Windows.UI.Xaml.Thickness(-pixelRadius / 2, -pixelRadius / 2, 0, 0);
+            }
+        }
+
+    }
+}
---
+++ DartaGramTablet/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Bing.Maps;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DartaGramTablet
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Geolocator geo = null;
+        GPSIcon gpspushpin;
+
+        public MainPage()
+        {
+
+
+            this.InitializeComponent();
+
+
+            gpspushpin = new GPSIcon(myMap)
+            {
+                Visibility = Windows.UI.Xaml.Visibility.Collapsed
+            };
+
+            myMap.Children.Add(gpspushpin);
+
+            //InitializeMap();
+            geomap();
+            DartaGramPin pin = null;
+            //Set the  location of the pin to the center of the map. 
+            //Bing.Maps.MapLayer.SetPosition(pin, myMap.Center);
+            foreach(Location location in getLocationList())
+            {
+                pin = new DartaGramPin();
+                MapLayer.SetPosition(pin, location);
+                myMap.Children.Add(pin);
+            }
+        }
+
+        public List<Location> getLocationList()
+        {
+            List<Location> locationsList = new List<Location>();
+            locationsList.Add(new Location() { Latitude = 41.26706, Longitude = -96.19755 });
+            locationsList.Add(new Location() { Latitude = 41.26811, Longitude = -96.19490 });
+            locationsList.Add(new Location() { Latitude = 41.26557, Longitude = -96.19697 });
+            return locationsList;
+        }
+
+        void geomap()
+        {
+            if (geo == null)
+            {
+                //Create an instance of the GeoLocator class.
+                geo = new Geolocator();
+            }
+
+            //Add the position changed event
+            geo.PositionChanged += geolocator_PositionChanged;
+
+
+        }
+        private void geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args)
+        {
+            // Need to get back onto UI thread before updating location information
+            this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(
+            () =>
+            {
+                //Get the current location
+                Location location = new Location(args.Position.Coordinate.Latitude, args.Position.Coordinate.Longitude);
+
+                //Update the position of the GPS pushpin
+                MapLayer.SetPosition(gpspushpin, location);
+
+                //Set the radius of the Accuracy Circle
+                gpspushpin.SetRadius(args.Position.Coordinate.Accuracy);
+
+                //Make GPS pushpin visible
+                gpspushpin.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+                //Update the map view to the current GPS location
+                myMap.SetView(location, 17);
+            }));
+        }
+    }
+}
